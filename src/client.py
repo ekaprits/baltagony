52c4cd1eff93ba3c618260ce31c7acf3eaf3039f
Manos Kapritsos <emkaprit@microsoft.com>
2015-11-12 20:30:06 -0800

    Added the Mastermind network library. Client-Server interaction code started.

--- src/client.py
+++ src/client.py
@@ -1,15 +1,57 @@
 #!/usr/bin/python
 
-import sys, socket, pickle
+import sys, socket, json, thread
 import pygame
 from pygame.locals import *
+from Mastermind import *
 
 suits = ['c', 'd', 'h', 's']
 facecards = ['j', 'q', 'k']
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 768
+CARD_WIDTH = 71
+CARD_HEIGHT = 96
+DECK_DISCARD_GAP = 20
+CARD_GAP = 20
+hand_rect = pygame.Rect((0,0),(0,0))
+deck_rect = pygame.Rect((0,0),(0,0))
+cTS = None
+client_timeout_connect = 5.0
+client_timeout_receive = 10.0
 
+class ClientTableState:
+    def __init__(self, myIndex, numPlayers, deckSize, hands, discardPile, numDraws, activePlayer, activeSuit, seven_streak):
+        self.myIndex = myIndex
+        self.numPlayers = numPlayers
+        self.deckSize = deckSize
+        self.hands = hands
+        self.discardPile = discardPile
+        self.numDraws = numDraws
+        self.activePlayer = activePlayer
+        self.activeSuit = activeSuit
+        self.seven_streak = seven_streak
+        
+def readStateFromServer(client, blocking=True):
+    global cTS
+    state_string = client.receive(blocking)
+    if state_string == None:
+        return False
+    state = json.loads(state_string)
+    cTS = ClientTableState(state[0], state[1], state[2],
+                            state[3], state[4], state[5],
+                            state[6], state[7], state[8])
+    if (cTS.activePlayer == cTS.myIndex):
+        pygame.display.set_caption("Baltagony (active)")
+    else:
+        pygame.display.set_caption("Baltagony (inactive)")
+    return True
+
+#def recvloop(sock):
+#    while True:
+#        readStateFromServer(sock)
+    
 def main(argv):
+    global cTS
     arg_len = len(sys.argv)
     if arg_len < 3:
         print("usage: client.py <server IP> <serverPort>")
@@ -17,38 +59,72 @@ def main(argv):
     
     serverIP = socket.gethostname()
     serverPort = int(sys.argv[2])
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((serverIP, serverPort))
-    hands_string = s.recv(512)
-    hands = pickle.loads(hands_string)
-    print(hands)
-    myIndex = hands[0]
-    #print("myindex = ", myindex)
-    allhands = hands[3]
-    discardPile = hands[4]
-    #myhand = hands[3][myindex]
-    #print("myhand is ", myhand)
-    
+    #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #s.connect((serverIP, serverPort))
+    client = MastermindClientTCP(client_timeout_connect,client_timeout_receive)
+    client.connect(serverIP, serverPort)
+    #data = client.receive()
+    readStateFromServer(client)
     
     pygame.init()
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), DOUBLEBUF)
     pygame.display.set_caption("Baltagony")
-    printAllHands(screen, allhands, myIndex)
-    printDiscardPile(screen, discardPile) 
+    #printAllHands(screen, allhands, myIndex)
+    #printDiscardPile(screen, discardPile)
+    #printDeck(screen, deckSize)
+    draw(screen)
+    #thread.start_new_thread(recvloop, (s, ))
+        
     while True:
+        existsNewState = True
+        if cTS.activePlayer != cTS.myIndex:
+            existsNewState = readStateFromServer(client, False)
+                
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-        pygame.display.update()
+            elif event.type == MOUSEBUTTONUP:
+                pos = event.pos
+                print("Mouse click at position ", pos)
+                print deck_rect
+                if deck_rect.collidepoint(pos):
+                    print "Clicked deck"
+                    lst = [0,0,0]
+                    send_string = json.dumps(lst)
+                    client.send(send_string)
+                    
+                elif hand_rect.collidepoint(pos):
+                    print "Clicked hand"
 
+        # print("Updating screen")
+        draw(screen)
+        existsNewState = False
+
+def draw(screen):
+    global cTS
+    screen.fill((255,255,255))
+    printAllHands(screen, cTS.hands, cTS.myIndex)
+    printDiscardPile(screen, cTS.discardPile)
+    printDeck(screen, cTS.deckSize)
+    pygame.display.update()
+    
+def printDeck(screen, deckSize):
+    global deck_rect
+    if (deckSize > 0):
+        deck = pygame.image.load('..\\media\\cards_gif\\b1fv.gif')
+        init_x = (WINDOW_WIDTH/2)-CARD_WIDTH-DECK_DISCARD_GAP
+        init_y = (WINDOW_HEIGHT-CARD_HEIGHT)/2
+        deck_rect = pygame.Rect((init_x,init_y),(init_x+CARD_WIDTH,init_y+CARD_HEIGHT))
+        screen.blit(deck, (init_x,init_y))
+    
 def printDiscardPile(screen, discardPile):
     cardOnTop = discardPile[-1:][0]
     imageFile = getFilenameFromCardNumber(cardOnTop)
-    myvar = pygame.image.load('..\\media\\cards_gif\\'+imageFile)
-    init_x = (WINDOW_WIDTH-71)/2
-    init_y = (WINDOW_HEIGHT-96)/2
-    screen.blit(myvar, (init_x,init_y))
+    image = pygame.image.load('..\\media\\cards_gif\\'+imageFile)
+    init_x = (WINDOW_WIDTH/2)+DECK_DISCARD_GAP
+    init_y = (WINDOW_HEIGHT-CARD_HEIGHT)/2
+    screen.blit(image, (init_x,init_y))
 
 def printAllHands(screen, hands, myIndex):
     printMyHand(screen, hands[myIndex])
@@ -58,32 +134,34 @@ def printAllHands(screen, hands, myIndex):
         nextIndex = (myIndex + 1 + i) % len(hands)
         numCards = hands[nextIndex]
         cardBack = pygame.image.load('..\\media\\cards_gif\\b1fv.gif')
-        estimated_width = 71 + (numCards*25)
-        height = 96
+        estimated_width = CARD_WIDTH + (numCards*CARD_GAP)
+        height = CARD_HEIGHT
         if i == 0:
             init_x =(3*WINDOW_WIDTH/4)-(estimated_width/2)
-            init_y = WINDOW_HEIGHT/2 - 96/2
+            init_y = WINDOW_HEIGHT/2 - CARD_HEIGHT/2
         elif i == 1:
             init_x =(WINDOW_WIDTH - estimated_width)/2
-            init_y = WINDOW_HEIGHT - 50 - 96
+            init_y = WINDOW_HEIGHT - 50 - CARD_HEIGHT
         elif i == 2:
             init_x =(WINDOW_WIDTH/4)-(estimated_width/2)
-            init_y = WINDOW_HEIGHT/2 - 96/2
+            init_y = WINDOW_HEIGHT/2 - CARD_HEIGHT/2
             
         for j in range(numCards):
             screen.blit(cardBack, (init_x+offset,init_y))
-            offset += 25
+            offset += CARD_GAP
             
 def printMyHand(screen, hand):
-    estimated_width = 71 + (len(hand)*25)
+    global hand_rect
+    estimated_width = CARD_WIDTH + (len(hand)*CARD_GAP)
     init_x =(WINDOW_WIDTH-estimated_width)/2
     init_y = 50
+    hand_rect = pygame.Rect((init_x, init_y),(init_x+estimated_width,init_y+CARD_WIDTH))
     offset = 0
     for card in hand:
         imageFile = getFilenameFromCardNumber(card)
         myvar = pygame.image.load('..\\media\\cards_gif\\'+imageFile)
         screen.blit(myvar, (init_x+offset,init_y))
-        offset += 25
+        offset += CARD_GAP
         #print(imageFile)
         
         
--- src/server.py
+++ src/server.py
@@ -1,14 +1,113 @@
 #!/usr/bin/python
 
-import sys, socket, pickle
+import sys, socket, json, threading
 from random import shuffle
+from Mastermind import *
+
+ACTION_DRAW = 0
+ACTION_PASS = 1
+ACTION_PLAY = 2
+
+connections = []
+numConnections = 0
+numPlayers = 0
+tableState = None
+
+class Server(MastermindServerTCP):
+    def __init__(self):
+        MastermindServerTCP.__init__(self, 0.5,0.5,10.0) #server refresh, connections' refresh, connection timeout
+
+        #self.chat = [None]*scrollback
+        self.mutex = threading.Lock()
+        '''
+        def add_message(self, msg):
+        timestamp = strftime("%H:%M:%S",gmtime())
+
+        self.mutex.acquire()
+        self.chat = self.chat[1:] + [timestamp+" | "+msg]
+        self.mutex.release()
+        '''
+    def callback_connect          (self                                          ):
+        print "Server connecting"
+        return super(Server,self).callback_connect()
+    def callback_disconnect       (self                                          ):
+        #Something could go here
+        return super(Server,self).callback_disconnect()
+    def callback_connect_client   (self, connection_object                       ):
+        global numConnections, connections, numPlayers, tableState
+        self.mutex.acquire()
+        if numConnections > numPlayers:
+            return
+        print "numConnections = ", numConnections
+        connections[numConnections] = connection_object
+
+        print "Client connected. Active connections: ", numConnections
+            
+        serialized_abstract_state = json.dumps(tableState.abstractState(numConnections))
+        print serialized_abstract_state
+        #connections[conn_index].socket.send([serialized_abstract_state])
+        self.callback_client_send(connection_object, serialized_abstract_state)
+        numConnections = numConnections + 1
+        if numConnections == numPlayers:
+            self.accepting_disallow()
+        
+        self.mutex.release()
+        return super(Server,self).callback_connect_client(connection_object)
+    def callback_disconnect_client(self, connection_object                       ):
+        #Something could go here
+        return super(Server,self).callback_disconnect_client(connection_object)
+
+    def callback_client_receive   (self, connection_object                       ):
+        #Something could go here
+        return super(Server,self).callback_client_receive(connection_object)
+    def callback_client_handle    (self, connection_object, data                 ):
+        action = json.loads(data)
+        print("Received player action ", action)
+        actionType = action[0]
+        card = action[1]
+        activeSuit = action[2]
+        
+        if actionType == ACTION_DRAW:
+            #card, deck = drawCardFromDeck(deck)
+            tableState.playerDrawsCard(tableState.activePlayer)
+        elif actionType == ACTION_PASS:
+            tableState.activePlayer = (tableState.activePlayer+1)%numPlayers
+        elif actionType == ACTION_PLAY:
+            tableState.playerPlaysCard(activePlayer, card, activeSuit)
+            tableState.activePlayer = (tableState.activePlayer+1)%numPlayers
+
+        for conn_index in range(numPlayers):
+            serialized_abstract_state = json.dumps(tableState.abstractState(conn_index))
+            self.callback_client_send(connections[conn_index], serialized_abstract_state)
+            #connections[conn_index].socket.send(serialized_abstract_state)
+
+        '''
+        cmd = data[0]
+        if cmd == "introduce":
+            self.add_message("Server: "+data[1]+" has joined.")
+        elif cmd == "add":
+            self.add_message(data[1])
+        elif cmd == "update":
+            pass
+        elif cmd == "leave":
+            self.add_message("Server: "+data[1]+" has left.")
+        '''
+        #self.callback_client_send(connection_object, self.chat)
+    def callback_client_send      (self, connection_object, data,compression=None):
+        #Something could go here
+        return super(Server,self).callback_client_send(connection_object, data,compression)
+
 class TableState:
-    def __init__(self, numPlayers, deck, hands, discardPile):
+    def __init__(self, numPlayers, deck, hands, discardPile, numDraws, activePlayer, activeSuit, seven_streak):
         self.numPlayers = numPlayers
         #self.initHandSize = initHandSize
         self.deck = deck
         self.hands = hands
         self.discardPile = discardPile
+        self.numDraws = numDraws
+        self.activePlayer = activePlayer
+        self.activeSuit = activeSuit
+        self.seven_streak = seven_streak
 
     def abstractHands(self, player):
         newlist = []
@@ -24,20 +123,39 @@ class TableState:
                 self.numPlayers,
                 len(self.deck),
                 self.abstractHands(player),
-                self.discardPile]
+                self.discardPile,
+                self.numDraws,
+                self.activePlayer,
+                self.activeSuit,
+                self.seven_streak]
 
+    def playerDrawsCard(self, player):
+        # TODO: shuffle when deck is empty
+        card, self.deck = drawCardFromDeck(self.deck)
+        self.hands[player].append(card)
+
+    def playerPlaysCard(self, player, card, activeSuit):
+        self.hands[player].remove(card)
+        self.discardPile.append(card)
+        if(card % 13 == 0):
+            self.activeSuit == activeSuit
+        
+        
 def main(argv):
+    global numConnections, connections, numPlayers, tableState
     arg_len = len(sys.argv)
     if arg_len < 2:
         print("usage: server.py <numPlayers>")
         sys.exit(2)
 
+    print "1"
     deckSize = 104
     initHandSize = 9
     numPlayers = int(sys.argv[1])
+    connections = [None] * numPlayers
     deck = range(deckSize)
     shuffle(deck)
-    print(deck)
+    print deck
 
     hands = []
 
@@ -51,40 +169,71 @@ def main(argv):
     #deal initial discard
     card, deck = drawCardFromDeck(deck)
     discardPile = [card]
+
+    numDraws = [0] * numPlayers
+    tableState = TableState(numPlayers, deck, hands, discardPile, numDraws, 0, 0, 0)
+    print tableState.hands
+    print tableState.deck 
+    print tableState.discardPile 
     
-    tableState = TableState(numPlayers, deck, hands, discardPile)
-    print(tableState.hands)
-    print(tableState.deck)
-    print(tableState.discardPile)
-    
-    connections = []
-    
+    '''
     s = socket.socket()         # Create a socket object
     host = socket.gethostname() # Get local machine name
     port = 12345                # Reserve a port for your service.
     s.bind((host, port))        # Bind to the port
     s.listen(5)                 # Now wait for client connection.:w
-
+    '''
     
+    server = Server()
+    host = socket.gethostname() # Get local machine name
+    print "before server connect"
+    server.connect(host, 12345)
+    print "after server connect"
+    server.accepting_allow()
+    '''
     for conn_count in range(numPlayers):
         remaining = numPlayers-conn_count
         print('Waiting for ', remaining, ' more connections')
         c, addr = s.accept()     # Establish connection with client.
         print('Got connection from', addr)
         connections.append([c, addr])
-
+    
+    while numConnections < numPlayers:
+        pass
+    
     print("All players connected!")
-    for conn_count in range(numPlayers):
-        #print conn_count
-        #print tableState.abstractState(conn_count)
-        serialized_abstract_state = pickle.dumps(tableState.abstractState(conn_count))
-        connections[conn_count][0].send(serialized_abstract_state)
-
+    for conn_index in range(numPlayers):
+        serialized_abstract_state = json.dumps(tableState.abstractState(conn_index))
+        print serialized_abstract_state
+        connections[conn_index].socket.send([serialized_abstract_state])
+    
     print("All hands sent")
     
+    while True:
+        print("Waiting for player action")
+        action_string = connections[tableState.activePlayer].socket.recv(128)
+        action = json.loads(action_string)
+        print("Received player action ", action)
+        actionType = action[0]
+        card = action[1]
+        activeSuit = action[2]
+        
+        if actionType == ACTION_DRAW:
+            #card, deck = drawCardFromDeck(deck)
+            tableState.playerDrawsCard(tableState.activePlayer)
+        elif actionType == ACTION_PASS:
+            tableState.activePlayer = (tableState.activePlayer+1)%numPlayers
+        elif actionType == ACTION_PLAY:
+            tableState.playerPlaysCard(activePlayer, card, activeSuit)
+            tableState.activePlayer = (tableState.activePlayer+1)%numPlayers
+
+        for conn_index in range(numPlayers):
+            serialized_abstract_state = json.dumps(tableState.abstractState(conn_index))
+            connections[conn_index].socket.send(serialized_abstract_state)
+        
         #c.send('Thank you for connecting')
         #c.close()                # Close the connection
-    
+    '''
 
 # naive implementation of draw.
 # TODO: if deck is empty, reshuffle discard pile into deck
